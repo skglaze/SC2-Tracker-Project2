70854ed0aafb37c3252914921e42de231647dc48
Sam Glaze <samglaze@Jimmys-MacBook-Pro.local>
2019-10-21 15:17:34 -0400

    edited my file to include the new routers

--- server.js
+++ server.js
@@ -2,51 +2,23 @@ const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
 
-const { templateRouter } = require('./controllers/template.js')
+const { userRouter } = require('./controllers/user.js')
+const { opponentRouter } = require('./controllers/opponent.js')
+const { mapRouter } = require('./controllers/map.js')
 
-
-/* Step 3
- *
- * Register middleware...
- */
-
-/* Step 3.a
- * ...to parse the body of the HTTP requests from a URL encoded string 
- */
 app.use(express.urlencoded({ extended: true }))
 
-/* Step 3.b 
- *
- * ...to parse the body of the HTTP requests from a JSON string  
- */
 app.use(express.json())
 
-/* Step 3.b 
- *
- * add method-override middleware for "faking" DELETE and PUT/PATCH requests 
- */
 app.use(methodOverride('_method'))
 
-/* Step 3.c
- *
- * use the `./public` directory to host static resources such as css and
- * image files 
- */
 app.use(express.static(__dirname + "/public"))
 
-/* Step 3.b
- *
- * set the view engine of express to use the hbs (handlebars) package 
- */
 app.set('view engine', 'hbs')
 
-
-/* Step 4
- *
- * add router for the application to use. The first argument is a prefix to all
- * the paths defined in the router.
- */
-app.use('/helloworld', templateRouter)
+app.use('/', userRouter)
+app.use('/', opponentRouter)
+app.use('/', mapRouter)
 
 /* Step 5
  *
